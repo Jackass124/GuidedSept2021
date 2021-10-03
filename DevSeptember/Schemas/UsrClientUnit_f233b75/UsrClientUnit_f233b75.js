207673f2c7be89716c5e436f301f56b2eb9821f7
Fuat Bogoutdinov <f_bogoutdinov@kbtu.kz>
2021-10-03 03:35:13 +0300

    Создание бп с преднастроенной страницей

---
+++ DevSeptember/Schemas/UsrClientUnit_f233b75/UsrClientUnit_f233b75.js
@@ -0,0 +1,52 @@
+define("UsrClientUnit_f233b75", [], function() {
+	return {
+		entitySchemaName: "",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "STRINGa2c84d6e-fbb7-4635-848b-7afa6f632f4a",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrTexr",
+					"enabled": true,
+					"isRequired": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP63a2292b-f5ab-4ec9-8591-d38d25f99e0d",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrLocation",
+					"enabled": true,
+					"contentType": 5,
+					"isRequired": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
